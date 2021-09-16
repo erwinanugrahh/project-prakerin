ca44b609eab15dcfb1b52dec63857fb67fa0b43f
indrarusmana19 <bryanofficial19@gmail.com>
2021-09-16 14:23:05 +0700

    melanjutkan pemasangan template user

--- public/user/js/script.js
+++ public/user/js/script.js
@@ -261,5 +261,16 @@ jQuery(function ($) {
 
 	});
 
+	$('.page-scroll').on('click', function(e) {
 
+		var tujuan = $(this).attr('href');
+	   
+		var elemenTujuan = $(tujuan);
+	   
+		$('html , body').animate({
+		 scrollTop: elemenTujuan.offset().top - 50
+		});
+	   
+		e.preventDefault();
+	});
 });
